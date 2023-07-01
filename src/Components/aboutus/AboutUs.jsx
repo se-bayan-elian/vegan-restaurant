fa342666f36eb51ae93d42caafacf513af5f5703
se-bayan-elian <bezo2024@gmail.com>
2023-07-01 05:33:46 +0300

    create about us section components

---
+++ src/Components/aboutus/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import SecHeader from "../global/secBtn/secheader/SecHeader";
+import Container from "../global/container/Container";
+import "./about.css";
+import { VEGAN_INFO } from "@/Mock/info";
+import SectionButton from "../global/secBtn/SectionButton";
+import Image from "next/image";
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <section className="about-sec">
+        <Container variant="md">
+          <div className="left">
+            <SecHeader
+              title="About Us"
+              desc={VEGAN_INFO.aboutUs.title}
+              variant="right"
+            />
+            <p>{VEGAN_INFO.aboutUs.description}</p>
+            <SectionButton content="Read more" href="" />
+          </div>
+          <div className="right">
+            <Image
+              src={VEGAN_INFO.aboutUs.img}
+              alt=""
+              width={378}
+              height={407}
+            />
+          </div>
+        </Container>
+      </section>
+    );
+  }
+}
