3bac5a9d27b08781d439d6c045fcf2293dce6980
se-bayan-elian <bezo2024@gmail.com>
2023-07-03 00:26:47 +0300

    fix animated background

--- src/app/MainComponent.jsx
+++ src/app/MainComponent.jsx
@@ -1,34 +1,36 @@
-'use client'
+"use client";
 import ContactSec from "@/Components/contact/ContactSec";
 import Footer from "@/Components/footer/Footer";
 import Header from "@/Components/header/Header";
 import SubscribeSec from "@/Components/subscribe/SubscribeSec";
 import React, { Component } from "react";
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter } from "next/navigation";
 
-const MainComponent = ({children}) => {
-  let params = usePathname()
-  console.log(params.includes('reserve'));
+const MainComponent = ({ children }) => {
+  let params = usePathname();
+  console.log(params.includes("reserve"));
   return (
     <main>
-        <Header variant={params.includes('reserve') || "withContent"} />
-        <div
-          className="animated-background"
-          style={{
-            background: 'url("/assets/animatedBg.gif") ',
-            backgroundSize: "cover",
-          }}
-        >
-          {children}
+      {/* if no in home page hide content  */}
+      <Header variant={params.includes("reserve") || "withContent"} />
+      {/* rest of page take animated background  */}
+      <div
+        className="animated-background"
+        style={{
+          background: 'url("/assets/animatedBg.gif") repeat',
+          backgroundSize: "cover",
+        }}
+      >
+        {children}
         {/* render footer  */}
         <footer className="main-footer">
           <ContactSec></ContactSec>
           <SubscribeSec></SubscribeSec>
           <Footer></Footer>
         </footer>
-        </div>
-      </main>
-  )
-}
+      </div>
+    </main>
+  );
+};
 
-export default MainComponent
+export default MainComponent;
